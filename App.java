9421daa6a85ed5b878dc64bb6fa801c72b4cf21c
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-08-08 17:19:20 +0300

    I have added code to check if the user wants to add a pet

--- App.java
+++ App.java
@@ -18,7 +18,40 @@ public class App{
                     System.out.println(individualPet.getColor());
 
                 }
-                
+
+            }
+            else if(navigationChoice.equals("search pets")){
+                System.out.println("WHAT TYPE OF PET ARE YOU LOOKING FOR?");
+                String newPet= myConsole.readLine();
+                System.out.println("HERE IS WHAT WE HAVE:");
+                for(PetStore individualPet: allPets){
+                    if(individualPet.animalIWant(newPet)){
+                        System.out.println("HERE ARE ALL OUR PETS");
+                    System.out.println("---------------------");
+                    System.out.println(individualPet.getName());
+                    System.out.println(individualPet.getType());
+                    System.out.println(individualPet.getColor());
+
+                    }
+                }
+            }
+            else if(navigationChoice.equals("add pet")){
+                System.out.println("ALRIGHT LET US ADD A PET:Enter its name:");
+                String petName=myConsole.readLine();
+                System.out.println("WHAT TYPE OF PET IS IT:");
+                String petType=myConsole.readLine();
+                System.out.println("WHAT COLOR IS THE PET:");
+                String petColor=myConsole.readLine();
+                PetStore userPet=new PetStore(petName,petType,petColor);
+                allPets.add(userPet);
+                System.out.println("here are your pet details");
+                System.out.println(userPet.getName());
+                System.out.println(userPet.getType());
+                System.out.println(userPet.getColor());
+
+            }
+            else{
+                System.out.println("COULDNT RECOGNIZE THAT");
             }
         }
         
